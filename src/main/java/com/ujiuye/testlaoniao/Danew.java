5c2aa1c300115c4331873010b7ef334c84507907
admin <2844911608@qq.com>
2018-12-05 21:41:42 +0800

    就喜欢欺负辛袅

---
+++ src/main/java/com/ujiuye/testlaoniao/Danew.java
@@ -0,0 +1,11 @@
+package com.ujiuye.testlaoniao;
+
+/**
+ * @author 天意无情
+ * @date 2018-12-05 21:40
+ */
+public class Danew {
+    public static void main(String[] args) {
+        System.out.println("喜欢欺负辛袅");
+    }
+}
