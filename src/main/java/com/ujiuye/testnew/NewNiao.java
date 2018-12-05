0f4a4fc54849400e721f9207301b3df47128fdb6
admin <2844911608@qq.com>
2018-12-05 21:31:34 +0800

    我是一只菜鸟

---
+++ src/main/java/com/ujiuye/testnew/NewNiao.java
@@ -0,0 +1,12 @@
+package com.ujiuye.testnew;
+
+/**
+ * @author 天意无情
+ * @date 2018-12-05 21:30
+ */
+public class NewNiao {
+
+    public static void main(String[] args) {
+        System.out.println("我是一只菜鸟");
+    }
+}
