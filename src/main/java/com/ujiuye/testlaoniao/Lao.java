71784181e394b71a011ec7ab58d5f1a8f5f24fc4
admin <2844911608@qq.com>
2018-12-05 21:39:40 +0800

    修改一下

--- src/main/java/com/ujiuye/testlaoniao/Lao.java
+++ src/main/java/com/ujiuye/testlaoniao/Lao.java
@@ -7,5 +7,6 @@ package com.ujiuye.testlaoniao;
 public class Lao {
     public static void main(String[] args) {
         System.out.println("我是一只老鸟");
+        System.out.println("我是一只菜鸟");
     }
 }
