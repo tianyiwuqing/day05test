308c148612952fd2ba42bdb2d8caf22aa7b578c9
admin <2844911608@qq.com>
2018-12-05 21:28:45 +0800

    老鸟添加一个方法

---
+++ src/main/java/com/ujiuye/testlaoniao/Lao.java
@@ -0,0 +1,11 @@
+package com.ujiuye.testlaoniao;
+
+/**
+ * @author 天意无情
+ * @date 2018-12-05 21:27
+ */
+public class Lao {
+    public static void main(String[] args) {
+        System.out.println("我是一只老鸟");
+    }
+}
